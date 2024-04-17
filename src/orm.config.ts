d875d670eb1bc0310c3067a310659439352ecf0c
brooktewabe <brooktewabe00@gmail.com>
2024-04-17 16:17:27 +0300

    'ticket-priority-count'

--- src/orm.config.ts
+++ src/orm.config.ts
@@ -1,10 +1,11 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
 export const config: TypeOrmModuleOptions = {
   type: 'postgres',
   username: 'postgres',
   password: '1234',
-  port: 5432,
-  host: '127.0.0.1',
+  port: +process.env.DB_PORT,
+  host: process.env.DB_HOST,
   database: 'ticket-tracker',
   synchronize: true,
   entities: ['dist/**/*.entity{.ts,.js}'],
